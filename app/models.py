90ce695e775ade0dbf7656a10f1c654896d9f387
Rodrigo <frs.rodrigobocayuva@gmail.com>
2025-11-10 15:04:21 -0300

    adding autentication

--- app/models.py
+++ app/models.py
@@ -1,5 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+from datetime import datetime, timezone
+
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    def set_password(self, password):
+        """Gera o hash da senha e armazena no campo password_hash."""
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        """Compara uma senha digitada com o hash armazenado."""
+        return bcrypt.check_password_hash(self.password_hash, password)
+    
+    def set_email(self, email):
+        self.email = email.strip().lower()
+
 
 class Livro(db.Model):
     # __tablename__ define o nome da tabela no banco de dados.
